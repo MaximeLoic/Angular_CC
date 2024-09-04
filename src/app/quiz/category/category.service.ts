2ca5dc5875ef7f7b023b2d270005978c682b47be
Paul-HenryN <ngpaulhenry@gmail.com>
2024-09-04 16:02:26 +0200

    feat: add category service

---
+++ src/app/quiz/category/category.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Category } from 'src/app/quiz/categories/category.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class CategoryService {
+  categories: Category[] = [];
+  constructor(private http: HttpClient) {}
+
+  getCategories() {
+    this.http.get<Category[]>('http://localhost:3000/categories').subscribe({
+      next: (categories) => {
+        this.categories = categories;
+      },
+      error: console.error,
+    });
+  }
+}
